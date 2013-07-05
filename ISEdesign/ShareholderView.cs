5a1c11e14d8aa38cf1060ba20e697ae897cb9667
Martz94 <finkel@intechinfo.fr>
2013-07-05 11:25:57 +0200

    Fuck jury

--- ISEdesign/ShareholderView.cs
+++ ISEdesign/ShareholderView.cs
@@ -59,7 +59,7 @@ namespace ISEdesign
                 i.SubItems.Add( s.Capital.ToString( "N2" ) );
                 i.SubItems.Add( s.Strategy.ToString() );
                 
-                //Coloring strats, not fucking working
+                //Coloring strats
                 if ( s.Strategy.ToString() == "Random Strategy" )      i.SubItems[4].ForeColor = System.Drawing.Color.Red;
                 else if ( s.Strategy.ToString() == "Stupid Strategy" ) i.SubItems[4].ForeColor = System.Drawing.Color.Green;
                 else if (s.Strategy.ToString() == "Smart Strategy" )   i.SubItems[4].ForeColor = System.Drawing.Color.Blue;
@@ -162,7 +162,92 @@ namespace ISEdesign
                     smartStrategyCapital += s.Capital;
                 }
             }
+            
+            decimal third;
+            decimal second;
+            decimal first;
 
+            if ( randomStrategyCapital > stupidStrategyCapital && randomStrategyCapital > smartStrategyCapital )
+            {
+                if ( stupidStrategyCapital > smartStrategyCapital )
+                //1 = Random
+                //2 = Stupid
+                //3 = Smart
+                {
+                    third = smartStrategyCapital;
+                    second = stupidStrategyCapital;
+                    first = randomStrategyCapital;
+
+                    smartStrategyCapital = first;
+                    stupidStrategyCapital = second;
+                    randomStrategyCapital = third;
+
+                }
+                else
+                //1 = Random
+                //2 = Smart
+                //3 = Stupid
+                {
+                    first = randomStrategyCapital;
+                    second = smartStrategyCapital;
+                    third = stupidStrategyCapital;
+
+                    smartStrategyCapital = first;
+                    stupidStrategyCapital = second;
+                    randomStrategyCapital = third;
+                }    
+            }
+            else if ( stupidStrategyCapital > randomStrategyCapital && stupidStrategyCapital > smartStrategyCapital )
+            {
+                if ( randomStrategyCapital > smartStrategyCapital )
+                //1 = Stupid
+                //2 = Random
+                //3 = Smart
+                {
+                    first = stupidStrategyCapital;
+                    second = randomStrategyCapital;
+                    third = smartStrategyCapital;
+
+                    smartStrategyCapital = first;
+                    stupidStrategyCapital = second;
+                    randomStrategyCapital = third;
+                }
+                else
+                //1 = Stupid
+                //2 = Smart
+                //3 = Random
+                {
+                    first = stupidStrategyCapital;
+                    second = smartStrategyCapital;
+                    third = randomStrategyCapital;
+
+                    smartStrategyCapital = first;
+                    stupidStrategyCapital = second;
+                    randomStrategyCapital = third;
+                }
+            }
+            else if ( smartStrategyCapital > randomStrategyCapital && smartStrategyCapital > stupidStrategyCapital )
+            {
+                if ( randomStrategyCapital > stupidStrategyCapital )
+                //1 = Smart
+                //2 = Random
+                //3 = Stupid
+                {
+                    first = smartStrategyCapital;
+                    second = randomStrategyCapital;
+                    third = stupidStrategyCapital;
+
+                    smartStrategyCapital = first;
+                    stupidStrategyCapital = second;
+                    randomStrategyCapital = third;
+                }
+                else
+                //1 = Smart
+                //2 = Stupid
+                //3 = Random
+                {
+                }
+            }
             string[] xValues = { "Random Strategy", "Stupid Strategy", "Smart Strategy" };
             double[] yValues = { (double)randomStrategyCapital, (double)stupidStrategyCapital, (double)smartStrategyCapital };
             serie1.Points.DataBindXY( xValues, yValues );
